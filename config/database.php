28d500ed7f2f67f5751645611aed18f2a35ce9e2
yumi_jkjn <yomnanaim@gmail.com>
2026-09-03 20:57:43 +0300

    merge the final page

--- config/database.php
+++ config/database.php
@@ -1,31 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-class Database {
-    private string $host    = 'localhost';
-    private string $db      = 'hotel_cafe';
-    private string $user    = 'root';
-    private string $pass    = '';
-    private string $charset = 'utf8mb4';
-    private ?PDO $pdo       = null;
-
-    public function connect(): PDO {
-        if ($this->pdo === null) {
-            $dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
-            $options = [
-                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES   => false,
-            ];
-            try {
-                $this->pdo = new PDO($dsn, $this->user, $this->pass, $options);
-            } catch (PDOException $e) {
-                die("Database Connection Failed: " . $e->getMessage());
-            }
-        }
-
-        return $this->pdo;
-=======
 namespace Core;
 
 use PDO;
@@ -75,6 +49,5 @@ class Database
         } else {
             return $result;
         }
->>>>>>> main
     }
 }
--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-<?php
-
-require "route.php";
-=======
+<?php require "route.php";?>
 <?php require("views/layouts/head.php"); ?>
 <?php require("views/layouts/header.php"); ?>
 <?php require("views/layouts/navbar.php"); ?>
@@ -11,4 +7,3 @@ require "route.php";
 
 <?php require("public/js/script.php"); ?> 
 <?php require("views/layouts/footer.php"); ?>
->>>>>>> main
