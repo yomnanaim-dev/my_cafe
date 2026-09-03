5aa6fcd18f24375d764e874909364a06072a5c0f
ataa <ataaabdulsamie@icloud.com>
2026-09-03 13:10:00 +0300

    add frontend

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php require("views/layouts/header.php"); ?>
 <?php require("views/layouts/navbar.php"); ?>
 
-
-
+<?php require("views/orders/cart.php"); ?>
 
 <?php require("public/js/script.php"); ?>
 <?php require("views/layouts/footer.php"); ?>
